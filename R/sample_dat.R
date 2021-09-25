e45ff31b556fadef753f543c7b6b04523f09dd79
junyzhou <junyzhou@iu.edu>
2021-09-25 00:53:43 -0400

    updates of data document

---
+++ R/sample_dat.R
@@ -0,0 +1,6 @@
+#' Sample toy data to illustrate the package usage
+#' @docType data
+#' @name sample_dat
+#' @format A list of 4 items. Z: observed binary outcomes, either 0 or 1. X: covariates; PI: population prevalence; true.prob: the true probablity of being positive (1) for each instance.
+#'
+NULL
